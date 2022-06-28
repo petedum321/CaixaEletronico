7c57368098f6eea4a24b84cb7024b0b1390d771e
Enzo Darre <enzo.darre@gmail.com>
2022-06-28 20:14:24 -0300

    First Commit

---
+++ NotasMoedas/CaixaEletronico.cs
@@ -0,0 +1,82 @@
+﻿
+namespace NotasMoedas
+{
+    class CaixaEletronico
+    {
+        public decimal Valor { get; set; }
+        public int NotaCem { get; set; }
+        public int NotaCinquenta { get; set; }
+        public int NotaVinte { get; set; }
+        public int NotaDez { get; set; }
+        public int NotaCinco { get; set; }
+        public int NotaDois { get; set; }
+        public int MoedaUm { get; set; }
+        public int MoedaCinquenta { get; set; }
+        public int MoedaVinteCinco { get; set; }
+        public int MoedaDez { get; set; }
+        public int MoedaCinco { get; set; }
+        public int MoedaZeroUm { get; set; }
+
+        public CaixaEletronico(decimal valor)
+        {
+            Valor = valor;
+        }
+
+        public void CalculaNotas()
+        {
+            NotaCem = (int)Valor / 100;
+            decimal resto = Valor - (NotaCem * 100);
+
+            NotaCinquenta = (int)resto / 50;
+            resto -= NotaCinquenta * 50;
+
+            NotaVinte = (int)resto / 20;
+            resto -= NotaVinte * 20;
+
+            NotaDez = (int)resto / 10;
+            resto -= NotaDez * 10;
+
+            NotaCinco = (int)resto / 5;
+            resto -= NotaCinco * 5;
+
+            NotaDois = (int)resto / 2;
+            resto -= NotaDois * 2;
+
+            MoedaUm = (int)resto / 1;
+            resto -= MoedaUm * 1;
+
+            MoedaCinquenta = (int)(resto / 0.5M);
+            resto -= MoedaCinquenta * 0.5M;
+
+            MoedaVinteCinco = (int)(resto / 0.25M);
+            resto -= MoedaVinteCinco * 0.25M;
+
+            MoedaDez = (int)(resto / 0.1M);
+            resto -= MoedaDez * 0.1M;
+
+            MoedaCinco = (int)(resto / 0.05M);
+            resto -= MoedaCinco * 0.05M;
+
+            MoedaZeroUm = (int)(resto / 0.01M);
+            resto -= MoedaZeroUm * 0.01M;
+        }
+
+        public override string ToString()
+        {
+            return $"NOTAS: \n" +
+                $"{NotaCem} nota(s) de R$ 100.00\n" +
+                $"{NotaCinquenta} nota(s) de R$ 50.00\n" +
+                $"{NotaVinte} nota(s) de R$ 20.00\n" +
+                $"{NotaDez} nota(s) de R$ 10.00\n" +
+                $"{NotaCinco} nota(s) de R$ 5.00\n" +
+                $"{NotaDois} nota(s) de R$ 2.00\n" +
+                $"MOEDAS:\n" +
+                $"{MoedaUm} moeda(s) de R$ 1.00\n" +
+                $"{MoedaCinquenta} moeda(s) de R$ 0.50\n" +
+                $"{MoedaVinteCinco} moeda(s) de R$ 0.25\n" +
+                $"{MoedaDez} moeda(s) de R$ 0.10\n" +
+                $"{MoedaCinco} moeda(s) de R$ 0.05\n" +
+                $"{MoedaZeroUm} moeda(s) de R$ 0.01\n";
+        }
+    }
+}
---
+++ NotasMoedas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NotasMoedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal valor = decimal.Parse(Console.ReadLine());
+
+            CaixaEletronico caixaEletronico = new CaixaEletronico(valor);
+
+            caixaEletronico.CalculaNotas();
+
+            Console.WriteLine(caixaEletronico);
+        }
+    }
+}
